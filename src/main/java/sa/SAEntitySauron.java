72492f406587840906db9bd8b5c24ede17e062f3
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-07-28 16:16:26 +0300

    Update SAEntitySauron.java

--- src/main/java/sa/SAEntitySauron.java
+++ src/main/java/sa/SAEntitySauron.java
@@ -5,20 +5,20 @@ import net.minecraft.world.World;
 
 public class SAEntitySauron
 extends LOTREntitySauron {
-    public SAEntitySauron(World world) {
-        super(world);
-    }
+	public SAEntitySauron(World world) {
+		super(world);
+	}
 
-    public void onLivingUpdate() {
-        super.onLivingUpdate();
-        if (!this.worldObj.isRemote && this.getHealth() < this.getMaxHealth() && this.ticksExisted % 10 == 0) {
-            this.setHealth(this.getHealth() - 2.0f);
-            this.heal(0.5f);
-        }
-    }
+	public void onLivingUpdate() {
+		super.onLivingUpdate();
+		if (!this.worldObj.isRemote && this.getHealth() < this.getMaxHealth() && this.ticksExisted % 10 == 0) {
+			this.setHealth(this.getHealth() - 2.0f);
+			this.heal(0.5f);
+		}
+	}
 
-    protected void applyEntityAttributes() {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(npcAttackDamage).setBaseValue(10.0);
-    }
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(npcAttackDamage).setBaseValue(10.0);
+	}
 }
