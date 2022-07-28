7945368d7f2cb7ce52e642f4bf54674ddeb1e59a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-07-28 16:16:47 +0300

    Update SA.java

--- src/main/java/sa/SA.java
+++ src/main/java/sa/SA.java
@@ -5,7 +5,6 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import lotr.common.entity.LOTREntities;
 
 @Mod(modid="sa", version="1.0", dependencies="required-after:lotr")
-
 public class SA {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
