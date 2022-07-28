765e4bf622e8dd6eb2d980b393ba2245a824ef86
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-07-28 16:15:40 +0300

    Update SA.java

--- src/main/java/sa/SA.java
+++ src/main/java/sa/SA.java
@@ -4,11 +4,11 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import lotr.common.entity.LOTREntities;
 
-@Mod(modid="sa", dependencies="required-after:lotr")
+@Mod(modid="sa", version="1.0", dependencies="required-after:lotr")
 
 public class SA {
 	@Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent e) {
-        LOTREntities.registerCreature(SAEntitySauron.class, "Sauron", 1000, 0, 0);
-    }
+	public void serverStarting(FMLServerStartingEvent e) {
+		LOTREntities.registerCreature(SAEntitySauron.class, "Sauron", 1000, 0, 0);
+	}
 }
