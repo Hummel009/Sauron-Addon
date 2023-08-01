d639825a2ff0a568c331e367bd10aca9c794d5f2
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-08-01 21:50:19 +0300

    2

---
+++ src/main/java/sa/SA.java
@@ -0,0 +1,13 @@
+package sa;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import lotr.common.entity.LOTREntities;
+
+@Mod(modid = "sa", dependencies = "required-after:lotr")
+public class SA {
+	@Mod.EventHandler
+	public void serverStarting(FMLServerStartingEvent e) {
+		LOTREntities.registerCreature(SAEntitySauron.class, "Sauron", 1000, 0, 0);
+	}
+}
