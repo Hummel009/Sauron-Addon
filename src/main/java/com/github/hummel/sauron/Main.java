cbc6eaefa6bd56dcd2f6c54c69bf3c450562c903
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-07 23:18:04 +0300

    Update Main.java

--- src/main/java/com/github/hummel/sauron/Main.java
+++ src/main/java/com/github/hummel/sauron/Main.java
@@ -4,8 +4,8 @@ import com.github.hummel.sauron.entity.EntitySauron;
 import com.github.hummel.sauron.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import lotr.common.entity.LOTREntities;
 
 @Mod(modid = "sauron", dependencies = "required-after:lotr", useMetadata = true)
@@ -14,7 +14,7 @@ public class Main {
 	private static CommonProxy proxy;
 
 	@Mod.EventHandler
-	public void serverStarting(FMLServerStartingEvent e) {
+	public void onInit(FMLInitializationEvent event) {
 		LOTREntities.registerCreature(EntitySauron.class, "Sauron", 1000, 0, 0);
 	}
 
