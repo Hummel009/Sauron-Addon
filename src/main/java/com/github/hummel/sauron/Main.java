e116247d01b32b95cf6165d8223768cdbcb42d42
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-14 21:17:12 +0300

    Update Main.java

--- src/main/java/com/github/hummel/sauron/Main.java
+++ src/main/java/com/github/hummel/sauron/Main.java
@@ -6,10 +6,11 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+@SuppressWarnings({"PublicField", "WeakerAccess"})
 @Mod(modid = "sauron", dependencies = "required-after:lotr", useMetadata = true)
 public class Main {
 	@SidedProxy(clientSide = "com.github.hummel.sauron.proxy.ClientProxy", serverSide = "com.github.hummel.sauron.proxy.ServerProxy")
-	private static CommonProxy proxy;
+	public static CommonProxy proxy;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
